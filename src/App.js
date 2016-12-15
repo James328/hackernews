6f49549c58ed090d18d0c0328b94df256fb02282
James Hume <jameshume328@gmail.com>
2016-12-14 18:25:15 -0600

    adding filter functionality

--- src/App.js
+++ src/App.js
@@ -20,11 +20,34 @@ const list = [
   },
 ];
 
+// ES6 sugar search function
+const isSearched = (query) => (item) => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+      query: '',
+    };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  onSearchChange(event) {
+    this.setState({ query: event.target.value });
+  }
+
   render() {
+    const { query, list } = this.state;
     return (
       <div className="App">
-        { list.map((item) =>
+        <form>
+          <input type="text" value={query} onChange={this.onSearchChange} />
+        </form>
+        { list.filter(isSearched(query)).map((item) =>
             <div key={item.objectID}>
               <span><a href={item.url}>{item.title}</a></span>
               <span>{item.author}</span>
